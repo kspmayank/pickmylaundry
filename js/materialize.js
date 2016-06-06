1886cfc4744719ad087681df21abcb147cb6e38f
Mayank <mayankkashyap95@gmail.com>
2016-06-06 15:57:47 +0530

    Completed

--- js/materialize.js
+++ js/materialize.js
@@ -5203,6 +5203,7 @@ function DatePicker( picker, settings ) {
                 includeToday = ':not(.' + settings.klass.buttonToday + ')'
             }
             picker.$root.find( 'button' + includeToday + ', select' ).attr( 'disabled', false )
+            picker.$root.appendTo('body');
         }, 1 ).
         on( 'close', function() {
             picker.$root.find( 'button, select' ).attr( 'disabled', true )
